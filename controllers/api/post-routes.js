0b190816c680f9b1ff944e11b46322cfcc483800
Noah Tidwell <ntidwell34@yahoo.com>
2021-12-04 12:34:58 -0600

    comment model and associations

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,6 +5,7 @@ const { User, Post } = require('../../models');
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'post_url', 'title', 'created_at'],
+      order: [['created_at', 'DESC']],
       include: [
         {
           model: User,
@@ -32,6 +33,59 @@ router.get('/', (req, res) => {
           attributes: ['username']
         }
       ]
+    })
+      .then(dbData => {
+        if (!dbData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+      Post.create({
+          title: req.body.title,
+          post_url: req.body.post_url,
+          user_id: req.body.user_id
+      })
+      .then(dbData => res.json(dbData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+  });
+
+  router.put('/:id', (req, res) => {
+    Post.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbData => {
+        if (!dbData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
     })
       .then(dbData => {
         if (!dbData) {
