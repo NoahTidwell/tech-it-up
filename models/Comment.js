c9506cfc903db2a7a74a181bd88b02301c110cef
Noah Tidwell <ntidwell34@yahoo.com>
2021-12-04 20:37:49 -0600

    fix delete post issue

--- models/Comment.js
+++ models/Comment.js
@@ -24,13 +24,13 @@ Comment.init(
           key: 'id'
         }
       },
-      post_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'post',
-          key: 'id'
-        }
-      }
+      // post_id: {
+      //   type: DataTypes.INTEGER,
+      //   references: {
+      //     model: 'post',
+      //     key: 'id'
+      //   }
+      // }
   },
   {
     sequelize,
